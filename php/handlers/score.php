33001851eac991b2d0d230ced97f599d2c47e0b3
danielemoschini <danielemoschini9@gmail.com>
2026-01-09 18:27:29 +0100

    Update score.php

--- php/handlers/score.php
+++ php/handlers/score.php
@@ -1,10 +1,27 @@
 <?php
 require 'config.php';
 
-if (isset($_SESSION['user_id'], $_POST['score'])) {
+$maxScore = [
+  'Facile' => 5,
+  'Normale' => 10,
+  'Difficile' => 15
+];
+
+if (isset($_SESSION['user_id'], $_POST['score'], $_POST['difficulty'])) {
+
+  //controllo modifiche js lato utente
+  $score = (int) $_POST['score'];
+  $allowed = ['Facile', 'Normale', 'Difficile'];
+  if (!in_array($_POST['difficulty'], $allowed, true)) { 
+    exit;
+  }
+  if ($score < 0 || $score > $maxScore[$_POST['difficulty']]) { 
+    exit;
+  }
+
   $stmt = $pdo->prepare(
-    "INSERT INTO scores (user_id, score) VALUES (?, ?)"
+    "INSERT INTO scores (user_id, difficulty, score) VALUES (?, ?, ?)"
   );
-  $stmt->execute([$_SESSION['user_id'], $_POST['score']]);
+  $stmt->execute([$_SESSION['user_id'], $_POST['difficulty'], $_POST['score']]);
 }
 ?>  
