90c092c68407f03538e860b80eed064e90b8413a
danielemoschini <danielemoschini9@gmail.com>
2026-01-09 18:26:53 +0100

    Update config.php

--- php/handlers/config.php
+++ php/handlers/config.php
@@ -3,6 +3,7 @@ $pdo = new PDO(
     "mysql:host=localhost;dbname=moschini_660071;charset=utf8",
     "root",
     "",
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false] //sicurezza per sql injection
 );
 session_start();
